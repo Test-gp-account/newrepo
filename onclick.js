537d67600a51148faf5ffec3b5391a6e86db06a7
Test-gp-account <33856034+Test-gp-account@users.noreply.github.com>
2017-11-23 14:32:33 +0530

    Update onclick.js

--- onclick.js
+++ onclick.js
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<button onclick="this.innerHTML=Date()"> The  today time is ?</button>
+<button onclick="this.innerHTML=Date()"> The  today time at master is ?</button>
 
 </body>
 </html>
